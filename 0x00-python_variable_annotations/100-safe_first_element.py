cb72d50b8c262c833bcb051b4ca70e3994e2599c
josfam <jamukun@proton.me>
2024-08-02 14:59:56 +0300

    Fix: Black, and documentation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-"""Duck typing an iterable/sequence"""
+"""Duck typing a general input and output"""
 
 from typing import Sequence, Any, Union
 
+
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Takes in a sequence with elements of any type, and returns either
+    an element of that sequence, or None"""
     if lst:
         return lst[0]
     else:
