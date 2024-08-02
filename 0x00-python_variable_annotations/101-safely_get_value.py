5f5cd9a90d036b87d06bdff23bd514372896d7ec
josfam <jamukun@proton.me>
2024-08-02 23:58:47 +0300

    Implement `11.More involved type annotations`

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Complex type annotations with TypeVar"""
+
+from typing import Any, Union, Mapping, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[T, None]:
+    """Takes a mapping, a key, and a default value.
+    Returns the value of the key, or the default value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
