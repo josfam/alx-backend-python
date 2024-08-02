42fa3257b9a2e7db665760a383f38f91c97cb728
josfam <jamukun@proton.me>
2024-08-03 00:40:07 +0300

    Implement `12. Type Checking`

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Takes in a tuple of values, and a factor, and returns the
+    same values, but multiplied by the factor
+    """
+    zoomed_in: List = [
+        item
+        for item in lst
+        for i in range(factor)
+        ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
