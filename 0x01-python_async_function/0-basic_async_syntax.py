80f748877003d7968c172f0f93429b940f6572ad
josfam <jamukun@proton.me>
2024-08-05 23:25:10 +0300

    Implement `0. The basics of async`

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Takes in an integer, and waits for a random delay between
+    0 and max_delay seconds and eventually returns how long the wait was"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """Takes in an integer, and waits for a random delay between
+    0 and max_delay seconds and eventually returns how long the wait was
+    """
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
