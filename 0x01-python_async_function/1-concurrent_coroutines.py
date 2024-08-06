33ca5f935ac8f6f989b67ab2b607b2cdadf329da
josfam <jamukun@proton.me>
2024-08-06 13:52:18 +0300

    Fix pycodestyle errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,9 +2,10 @@
 
 """Spawning different co-routines, and sorting their delay times"""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-from typing import List
 import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
