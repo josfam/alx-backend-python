8610bb280569c6730c789eff7f5047f544849bc1
josfam <jamukun@proton.me>
2024-08-06 04:32:42 +0300

    Fix: Import asyncio

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-
+import asyncio
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Takes an int and a delay, then spawns a wait_random co-routine n times.
