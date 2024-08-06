dcb8123844ae75ec71ffcb1e1c71d1485511c516
josfam <jamukun@proton.me>
2024-08-06 15:08:59 +0300

    Implement `3. Tasks`

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""""""
+"""Returning a Task object"""
 
 import asyncio
 
@@ -8,5 +8,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """ """
+    """
+    Args:
+        max_delay: The maximum time for a pause of the co-routine
+
+    Returns:
+        A Task object
+    """
     return asyncio.create_task(wait_random(max_delay))
